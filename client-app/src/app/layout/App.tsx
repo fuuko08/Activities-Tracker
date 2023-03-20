3cf648ffdef75c1f697b498fdd10c848bee0b465
Selina Su <phongsu20@gmail.com>
2023-03-19 17:04:24 -0700

    edit mode is working

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,15 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Container, Header, List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import {v4 as uuid} from 'uuid';
 
 function App() {
   const [activities, setActivites] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities')
@@ -15,11 +18,45 @@ function App() {
       })
   }, []);
 
+  function handleSelectedActivity(id: string) {
+    setSelectedActivity(activities.find(x => x.id === id));
+  };
+
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  };
+
+  function handleFormOpen(id?: string) {
+    id ? handleSelectedActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  };
+
+  function handleFormClose() {
+    setEditMode(false);
+  };
+
+  function handleCreateOrEditActivity(activity: Activity) {
+    activity.id 
+      ? setActivites([...activities.filter(x => x.id !== activity.id), activity])
+      : setActivites([...activities, {...activity, id: uuid()}]);
+    setEditMode(false);
+    setSelectedActivity(activity);
+  };
+
   return (
     <>
-      <NavBar />
+      <NavBar openForm={handleFormOpen}/>
       <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard activities={activities}/>
+        <ActivityDashboard 
+          activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectedActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose} 
+          editMode={editMode}  
+          createOrEdit={handleCreateOrEditActivity}      
+        />
       </Container>        
     </>
   );
