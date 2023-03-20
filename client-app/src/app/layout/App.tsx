d1c5f6409dd41a903b229c4b6c27cccd2b88c811
Selina Su <phongsu20@gmail.com>
2023-03-20 13:22:43 -0700

    CRUD all working

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,20 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
   const [activities, setActivites] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities')
+    agent.Activities.list()
       .then(response => {
-        setActivites(response.data);
+        let activities: Activity[] = [];
+        response.forEach(activity => {
+          activity.date = activity.date.split('T')[0];
+          activities.push(activity);
+        })
+        setActivites(activities);
+        setLoading(false);
       })
   }, []);
 
@@ -36,16 +45,34 @@ function App() {
   };
 
   function handleCreateOrEditActivity(activity: Activity) {
-    activity.id 
-      ? setActivites([...activities.filter(x => x.id !== activity.id), activity])
-      : setActivites([...activities, {...activity, id: uuid()}]);
-    setEditMode(false);
-    setSelectedActivity(activity);
+    setSubmitting(true);
+    if(activity.id) {
+      agent.Activities.update(activity).then(() => {
+        setActivites([...activities.filter(x => x.id !== activity.id), activity])
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    } else {
+      activity.id = uuid();
+      agent.Activities.create(activity).then(() => {
+      setActivites([...activities, activity]) 
+      setSelectedActivity(activity);
+      setEditMode(false);
+      setSubmitting(false);
+      })
+    }
   };
 
   function handleDeleteActivity(id: string) {
-    setActivites([...activities.filter(x => x.id !== id)])
+    setSubmitting(true);
+    agent.Activities.delete(id).then(() => {
+      setActivites([...activities.filter(x => x.id !== id)]);
+      setSubmitting(false);
+    })   
   };
+
+  if(loading) return <LoadingComponent content='Loading app' />
 
   return (
     <>
@@ -60,7 +87,8 @@ function App() {
           closeForm={handleFormClose} 
           editMode={editMode}  
           createOrEdit={handleCreateOrEditActivity}  
-          deleteActivity={handleDeleteActivity}    
+          deleteActivity={handleDeleteActivity} 
+          submitting={submitting}   
         />
       </Container>        
     </>
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,10 +15,12 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({activities, selectedActivity, 
-        selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props) {
+        selectActivity, cancelSelectActivity, editMode, openForm, closeForm, 
+        createOrEdit, deleteActivity, submitting}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
@@ -26,6 +28,7 @@ export default function ActivityDashboard({activities, selectedActivity,
                     activities={activities} 
                     selectActivity={selectActivity}
                     deleteActivity={deleteActivity}
+                    submitting={submitting}
                 />
             </Grid.Column>
             <Grid.Column width='6'>
@@ -40,6 +43,7 @@ export default function ActivityDashboard({activities, selectedActivity,
                     closeForm={closeForm} 
                     activity={selectedActivity} 
                     createOrEdit={createOrEdit}
+                    submitting={submitting}
                 />}
             </Grid.Column>
         </Grid>
