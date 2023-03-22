cbc828b7946acf8968430ecb17286fe0e8ea8356
Selina Su <phongsu20@gmail.com>
2023-03-21 20:19:55 -0700

    add styling

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,6 +4,7 @@ import ActivityList from './ActivityList';
 import { useStore } from '../../../app/stores/store';
 import { observer } from "mobx-react-lite";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import ActivityFilters from './ActivityFilters';
 
 export default observer (function ActivityDashboard() {
     const {activityStore} = useStore();
@@ -11,7 +12,7 @@ export default observer (function ActivityDashboard() {
 
     useEffect(() => {
         if (activityRegistry.size === 0) loadActivities();
-    }, [loadActivities])
+    }, [loadActivities, activityRegistry.size])
 
     if (activityStore.loadingInitial) return <LoadingComponent content="Loading app..." />
 
@@ -21,7 +22,7 @@ export default observer (function ActivityDashboard() {
                 <ActivityList />               
             </Grid.Column>
             <Grid.Column width='6'>
-                <h2>Activity Filters</h2>               
+                <ActivityFilters />              
             </Grid.Column>
         </Grid>
     )
