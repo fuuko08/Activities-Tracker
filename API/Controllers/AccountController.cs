6cf78a6c829d29ec358bd43c99f7342248797156
Selina Su <phongsu20@gmail.com>
2023-03-29 20:45:54 -0700

    use cloudinary to upload pic

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -22,11 +22,11 @@ namespace API.Controllers
             _tokenService = tokenService;
         }
 
-        [AllowAnonymous]
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
 		{
-			var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            var user = await _userManager.Users.Include(p => p.Photos)
+                .FirstOrDefaultAsync(x => x.Email == loginDto.Email);
 
 			if (user == null) return Unauthorized();
 
@@ -39,7 +39,6 @@ namespace API.Controllers
 			return Unauthorized();
 		}
 
-        [AllowAnonymous]
         [HttpPost("register")]
 		public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
 		{
@@ -76,7 +75,8 @@ namespace API.Controllers
 		[HttpGet]
         public async Task<ActionResult<UserDto>> GetCurrentUser()
 		{
-			var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+            var user = await _userManager.Users.Include(p => p.Photos)
+                .FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
 
 			return CreateUserObject(user);
 		}
@@ -86,7 +86,7 @@ namespace API.Controllers
             return new UserDto
             {
                 DisplayName = user.DisplayName,
-                Image = null,
+                Image = user?.Photos?.FirstOrDefault(x => x.IsMain)?.Url,
                 Token = _tokenService.CreateToken(user),
                 UserName = user.UserName
             };
---
+++ Infrastructure/Photos/CloudinarySetting.cs
@@ -0,0 +1,28 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace Infrastructure.Photos
+{
+	public class CloudinarySettings
+	{
+		public string CloudName { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+
+        internal Task DestroyAsync(DeletionParams deleteParams)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Task UploadAsync(ImageUploadParams uploadParams)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static implicit operator CloudinarySettings(Cloudinary v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
